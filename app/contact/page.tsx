06da91f94703c42f9300c8a201c8e8049fa46625
Tushar Goyal <gtushar132@gmail.com>
2025-04-28 10:41:16 +0530

    contact update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -331,7 +331,7 @@ export default function ContactPage() {
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
                     </svg>
                     <a href="tel:+919876543210" className="hover:text-gray-900 transition-colors">
-                      +91 98765 43210
+                      +91 7892637729,+91 7337681055
                     </a>
                   </p>
                   <p className="flex items-center text-gray-600">
